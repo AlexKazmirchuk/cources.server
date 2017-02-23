f1da4aa0586e7c017fc65bc3c1264a8ae5013056
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-02-23 15:06:55 +0200

    added RegisterRequestHandler for handle register command

---
+++ softgroup/services/authorization/authorization-imp/src/main/java/com/softgroup/authorithation/impl/handler/RegisterRequestHandler.java
@@ -0,0 +1,31 @@
+package com.softgroup.authorithation.impl.handler;
+
+import com.softgroup.authorization.api.message.RegisterRequest;
+import com.softgroup.authorization.api.message.RegisterResponse;
+import com.softgroup.authorization.api.router.AuthorizationRequestHandler;
+import com.softgroup.common.protocol.Request;
+import com.softgroup.common.protocol.Response;
+import com.softgroup.common.router.api.AbstractRequestHandler;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by sasha on 23.02.17.
+ */
+
+@Component
+public class RegisterRequestHandler <T extends RegisterRequest, R extends RegisterResponse> extends AbstractRequestHandler implements AuthorizationRequestHandler {
+
+    @Override
+    public String getName() {
+        return "register";
+    }
+
+    @Override
+    public Response handle(Request msg) {
+        // implement later
+
+
+        //
+        return null;
+    }
+}
