01f9825ffac8387cab8179dbf16e2031944ccaeb
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-02-23 12:33:35 +0200

    completed RegisterRequest and RegisterResponse classes

--- softgroup/services/authorization/authorization-api/src/main/java/com/softgroup/authorization/api/message/RegisterRequest.java
+++ softgroup/services/authorization/authorization-api/src/main/java/com/softgroup/authorization/api/message/RegisterRequest.java
@@ -8,4 +8,32 @@ import com.softgroup.common.protocol.RequestData;
  */
 public class RegisterRequest implements RequestData{
 	private static final long serialVersionUID = -645554380912935546L;
+
+	private String phoneNumber;
+	private String localeCode;
+	private Integer deviceID;
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getLocaleCode() {
+		return localeCode;
+	}
+
+	public void setLocaleCode(String localeCode) {
+		this.localeCode = localeCode;
+	}
+
+	public Integer getDeviceID() {
+		return deviceID;
+	}
+
+	public void setDeviceID(Integer deviceID) {
+		this.deviceID = deviceID;
+	}
 }
--- softgroup/services/authorization/authorization-api/src/main/java/com/softgroup/authorization/api/message/RegisterResponse.java
+++ softgroup/services/authorization/authorization-api/src/main/java/com/softgroup/authorization/api/message/RegisterResponse.java
@@ -8,4 +8,32 @@ import com.softgroup.common.protocol.ResponseData;
  */
 public class RegisterResponse implements ResponseData {
 	private static final long serialVersionUID = -5146888202653750948L;
+
+	private Integer registrationRequestUuid;
+	private Integer registrationTimeoutSec;
+	private Integer authCode;
+
+	public Integer getRegistrationRequestUuid() {
+		return registrationRequestUuid;
+	}
+
+	public void setRegistrationRequestUuid(Integer registrationRequestUuid) {
+		this.registrationRequestUuid = registrationRequestUuid;
+	}
+
+	public Integer getRegistrationTimeoutSec() {
+		return registrationTimeoutSec;
+	}
+
+	public void setRegistrationTimeoutSec(Integer registrationTimeoutSec) {
+		this.registrationTimeoutSec = registrationTimeoutSec;
+	}
+
+	public Integer getAuthCode() {
+		return authCode;
+	}
+
+	public void setAuthCode(Integer authCode) {
+		this.authCode = authCode;
+	}
 }
