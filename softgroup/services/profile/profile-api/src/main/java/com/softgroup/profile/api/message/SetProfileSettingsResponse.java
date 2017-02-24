e74e6c77732b907b10e7c9df716c56cf6ecdfb51
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-02-24 14:16:36 +0200

    added SetProfileSettingsRequest and SPSResponse classes

---
+++ softgroup/services/profile/profile-api/src/main/java/com/softgroup/profile/api/message/SetProfileSettingsResponse.java
@@ -0,0 +1,11 @@
+package com.softgroup.profile.api.message;
+
+import com.softgroup.common.protocol.ResponseData;
+
+/**
+ * Created by sasha on 24.02.17.
+ */
+public class SetProfileSettingsResponse implements ResponseData {
+
+    private static final long serialVersionUID = 1958853119600055138L;
+}
