133d9cb38726368915bd5794b040ec59bb80eade
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-02-24 14:48:27 +0200

    added interfaces: ProfileRequestHandler, ProfileRouterHandler

---
+++ softgroup/services/profile/profile-api/src/main/java/com/softgroup/profile/api/router/ProfileRequestHandler.java
@@ -0,0 +1,9 @@
+package com.softgroup.profile.api.router;
+
+import com.softgroup.common.router.api.RequestHandler;
+
+/**
+ * Created by sasha on 24.02.17.
+ */
+public interface ProfileRequestHandler extends RequestHandler {
+}
---
+++ softgroup/services/profile/profile-api/src/main/java/com/softgroup/profile/api/router/ProfileRouterHandler.java
@@ -0,0 +1,10 @@
+package com.softgroup.profile.api.router;
+
+import com.softgroup.common.router.api.CommonRouterHandler;
+import com.softgroup.common.router.api.RouterHandler;
+
+/**
+ * Created by sasha on 24.02.17.
+ */
+public interface ProfileRouterHandler extends RouterHandler, CommonRouterHandler {
+}
