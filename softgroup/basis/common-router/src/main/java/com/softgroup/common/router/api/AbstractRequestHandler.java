25c28cae16db52f876ba3eab06dc55d3ac13c4f8
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-02-27 09:02:11 +0200

    changed AbstractRequestHandler, added new method, and body for parsing requestData

--- softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRequestHandler.java
+++ softgroup/basis/common-router/src/main/java/com/softgroup/common/router/api/AbstractRequestHandler.java
@@ -1,20 +1,33 @@
 package com.softgroup.common.router.api;
 
-
+import com.softgroup.common.datamapper.JacksonDataMapper;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.RequestData;
 import com.softgroup.common.protocol.Response;
 import com.softgroup.common.protocol.ResponseData;
 
-public class AbstractRequestHandler<T extends RequestData, R extends ResponseData> implements RequestHandler {
-	@Override
-	public String getName() {
-		return null;
-	}
+import java.lang.reflect.ParameterizedType;
+import java.util.Map;
+
+public abstract class AbstractRequestHandler
+		<T extends RequestData, R extends ResponseData>
+		implements RequestHandler {
 
 	@Override
 	public Response<R> handle(Request<?> msg) {
-		return null;
+		Request<T> request = new Request<>();
+		request.setHeader(msg.getHeader());
+
+		// todo check if working in future
+		Class<T> genericClass = (Class<T>) ((ParameterizedType) this.getClass()
+				.getGenericSuperclass())
+				.getActualTypeArguments()[0];
+		JacksonDataMapper mapper = new JacksonDataMapper();
+		request.setData(mapper.convert((Map<String, Object>) msg.getData(), genericClass));
+
+		return doHandle(request);
 	}
+
+	public abstract Response<R> doHandle(Request<T> msg);
 
 }
