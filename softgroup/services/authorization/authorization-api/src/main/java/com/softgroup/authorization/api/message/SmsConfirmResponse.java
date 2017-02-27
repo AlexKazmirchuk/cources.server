fbdcfc7a5deded59377ecd1334df74dea982749e
Alexander Kazmirchuk <zedomo@mail.ru>
2017-02-27 09:43:09 +0200

    Adding new classes to authorization-api module (#7)
    * added classes for storing request and response data for authorization type
    
    * changed AuthorizationRouterHandler

---
+++ softgroup/services/authorization/authorization-api/src/main/java/com/softgroup/authorization/api/message/SmsConfirmResponse.java
@@ -0,0 +1,20 @@
+package com.softgroup.authorization.api.message;
+
+import com.softgroup.common.protocol.ResponseData;
+
+/**
+ * Created by sasha on 23.02.17.
+ */
+public class SmsConfirmResponse implements ResponseData {
+    private static final long serialVersionUID = 4895238817730981751L;
+
+    private String deviceToken;
+
+    public String getDeviceToken() {
+        return deviceToken;
+    }
+
+    public void setDeviceToken(String deviceToken) {
+        this.deviceToken = deviceToken;
+    }
+}
