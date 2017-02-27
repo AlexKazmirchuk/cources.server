e72a79fde9248d113a41ca64454ec2867b265055
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-02-27 15:00:47 +0200

    added  base classes  in profile-impl module

---
+++ softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/handler/GetLastTimeOnlineRequestHandler.java
@@ -0,0 +1,34 @@
+package com.softgroup.profile.impl.handler;
+
+import com.softgroup.common.protocol.Request;
+import com.softgroup.common.protocol.Response;
+import com.softgroup.common.router.api.AbstractRequestHandler;
+import com.softgroup.profile.api.message.GetLastTimeOnlineRequest;
+import com.softgroup.profile.api.message.GetLastTimeOnlineResponse;
+import com.softgroup.profile.api.router.ProfileRequestHandler;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author AlexKazmirchuk
+ * @since 24.02.17.
+ */
+
+@Component
+public class GetLastTimeOnlineRequestHandler
+        extends AbstractRequestHandler<GetLastTimeOnlineRequest,GetLastTimeOnlineResponse>
+        implements ProfileRequestHandler {
+
+    @Override
+    public String getName() {
+        return "get_last_time_online";
+    }
+
+    @Override
+    public Response<GetLastTimeOnlineResponse> doHandle(Request<GetLastTimeOnlineRequest> msg) {
+        // todo implement later
+
+
+        //
+        return null;
+    }
+}
---
+++ softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/handler/GetProfileSettingsRequestHandler.java
@@ -0,0 +1,33 @@
+package com.softgroup.profile.impl.handler;
+
+import com.softgroup.common.protocol.Request;
+import com.softgroup.common.protocol.Response;
+import com.softgroup.common.router.api.AbstractRequestHandler;
+import com.softgroup.profile.api.message.GetProfileSettingsRequest;
+import com.softgroup.profile.api.message.GetProfileSettingsResponse;
+import com.softgroup.profile.api.router.ProfileRequestHandler;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author AlexKazmirchuk
+ * @since 24.02.17.
+ */
+
+@Component
+public class GetProfileSettingsRequestHandler
+        extends AbstractRequestHandler<GetProfileSettingsRequest,GetProfileSettingsResponse>
+        implements ProfileRequestHandler {
+    @Override
+    public String getName() {
+        return "get_profile_settings";
+    }
+
+    @Override
+    public Response<GetProfileSettingsResponse> doHandle(Request<GetProfileSettingsRequest> msg) {
+        // todo implement later
+
+
+        //
+        return null;
+    }
+}
