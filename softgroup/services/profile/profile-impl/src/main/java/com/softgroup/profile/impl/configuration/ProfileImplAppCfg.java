41f01039c389813f483a793226edb99755964679
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-03-02 18:35:19 +0200

    added configuration classes to auth , profile, messenger impl modules

---
+++ softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/configuration/ProfileImplAppCfg.java
@@ -0,0 +1,14 @@
+package com.softgroup.profile.impl.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author AlexKazmirchuk
+ * @since 02.03.17.
+ */
+
+@Configuration
+@ComponentScan("com.softgroup.profile.impl")
+public class ProfileImplAppCfg {
+}
