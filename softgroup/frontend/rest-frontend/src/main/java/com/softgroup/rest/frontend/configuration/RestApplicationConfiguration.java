4893a8a48141be2844735379b75ee8ec50686d9e
Alexander Kazmirchuk <zedomo@mail.ru>
2017-03-16 16:32:01 +0200

    added rest module , rest configuration classes and test controller (#20)
    * added rest module , rest configuration classes and test controller
    
    * added base rest controller, deleted test controller

---
+++ softgroup/frontend/rest-frontend/src/main/java/com/softgroup/rest/frontend/configuration/RestApplicationConfiguration.java
@@ -0,0 +1,24 @@
+package com.softgroup.rest.frontend.configuration;
+
+import com.softgroup.authorization.impl.configuration.AuthorizationImplAppCfg;
+import com.softgroup.common.datamapper.configuration.DataMapperAppCfg;
+import com.softgroup.common.router.impl.configuration.FirstRouterAppCfg;
+import com.softgroup.messenger.impl.configuration.MessengerImplAppCfg;
+import com.softgroup.profile.impl.configuration.ProfileImplAppCfg;
+import org.springframework.context.annotation.*;
+
+
+@Configuration
+@ComponentScan(basePackages = "com.softgroup.rest.frontend",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
+                value = Configuration.class))
+@Import(value = {
+        DataMapperAppCfg.class,
+        AuthorizationImplAppCfg.class,
+        ProfileImplAppCfg.class,
+        MessengerImplAppCfg.class,
+        FirstRouterAppCfg.class})
+
+public class RestApplicationConfiguration {
+
+}
