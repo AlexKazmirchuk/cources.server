90f234960e13ef05992265657455cec2170d6469
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-03-17 20:01:38 +0200

    updated a few model classes

--- softgroup/basis/common-dao/common-dao-api/src/main/java/com/softgroup/common/dao/api/entities/ProfileStatusEntity.java
+++ softgroup/basis/common-dao/common-dao-api/src/main/java/com/softgroup/common/dao/api/entities/ProfileStatusEntity.java
@@ -1,5 +1,6 @@
 package com.softgroup.common.dao.api.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.io.Serializable;
@@ -15,9 +16,46 @@ public class ProfileStatusEntity extends BaseEntity implements Serializable {
 
     private static final long serialVersionUID = -5692872742644522060L;
 
-    //todo add other fields later
+    @Column(name = "is_online")
+    private Boolean isOnline;
+
+    @Column(name = "last_time_online")
+    private Long lastTimeOnline;
 
     public ProfileStatusEntity() {
     }
 
+    public Boolean getOnline() {
+        return isOnline;
+    }
+
+    public void setOnline(Boolean online) {
+        isOnline = online;
+    }
+
+    public Long getLastTimeOnline() {
+        return lastTimeOnline;
+    }
+
+    public void setLastTimeOnline(Long lastTimeOnline) {
+        this.lastTimeOnline = lastTimeOnline;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ProfileStatusEntity)) return false;
+
+        ProfileStatusEntity that = (ProfileStatusEntity) o;
+
+        if (isOnline != null ? !isOnline.equals(that.isOnline) : that.isOnline != null) return false;
+        return lastTimeOnline != null ? lastTimeOnline.equals(that.lastTimeOnline) : that.lastTimeOnline == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = isOnline != null ? isOnline.hashCode() : 0;
+        result = 31 * result + (lastTimeOnline != null ? lastTimeOnline.hashCode() : 0);
+        return result;
+    }
 }
