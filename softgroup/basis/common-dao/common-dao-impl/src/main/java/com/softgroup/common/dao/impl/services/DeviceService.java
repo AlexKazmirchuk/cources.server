166d41756f37295ae244fd0bf504cd18ed20e9b0
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-03-19 14:42:11 +0200

    added repository and service for DeviceEntity

---
+++ softgroup/basis/common-dao/common-dao-impl/src/main/java/com/softgroup/common/dao/impl/services/DeviceService.java
@@ -0,0 +1,13 @@
+package com.softgroup.common.dao.impl.services;
+
+import com.softgroup.common.dao.api.entities.DeviceEntity;
+import com.softgroup.common.dao.impl.repositories.DeviceRepository;
+
+/**
+ * @author AlexKazmirchuk
+ * @since 19.03.17.
+ */
+
+public class DeviceService extends
+        CommonService<DeviceEntity,DeviceRepository>  {
+}
