840eab606c5a87a78ad0032d7fc61c449dd151ad
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-03-21 13:18:48 +0200

    some small changes

--- softgroup/basis/common-dao/common-dao-impl/src/main/java/com/softgroup/common/dao/impl/services/DeviceService.java
+++ softgroup/basis/common-dao/common-dao-impl/src/main/java/com/softgroup/common/dao/impl/services/DeviceService.java
@@ -2,12 +2,14 @@ package com.softgroup.common.dao.impl.services;
 
 import com.softgroup.common.dao.api.entities.DeviceEntity;
 import com.softgroup.common.dao.impl.repositories.DeviceRepository;
+import org.springframework.stereotype.Service;
 
 /**
  * @author AlexKazmirchuk
  * @since 19.03.17.
  */
 
+@Service
 public class DeviceService extends
         CommonService<DeviceEntity,DeviceRepository>  {
 }
