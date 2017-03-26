e0683b6027fafc2d09bfaf1b1de90ba2f6e15174
Alexander Kazmirchuk <zedomo@mail.ru>
2017-03-26 22:30:36 +0300

    Small common changes  (#26)
    * added profileID and deviceID fields to RoutedAction
    
    * added new controller AuthController for handling public requests and other small changes

--- softgroup/basis/common-protocol/src/main/java/com/softgroup/common/protocol/RoutedAction.java
+++ softgroup/basis/common-protocol/src/main/java/com/softgroup/common/protocol/RoutedAction.java
@@ -5,4 +5,23 @@ import java.io.Serializable;
 public class RoutedAction<T extends Serializable> extends Action<T> {
 	private static final long serialVersionUID = -318012176965757271L;
 
+	private String profileID;
+
+	private String deviceID;
+
+	public String getProfileID() {
+		return profileID;
+	}
+
+	public void setProfileID(String profileID) {
+		this.profileID = profileID;
+	}
+
+	public String getDeviceID() {
+		return deviceID;
+	}
+
+	public void setDeviceID(String deviceID) {
+		this.deviceID = deviceID;
+	}
 }
