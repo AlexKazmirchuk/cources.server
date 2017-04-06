0362b64e130acba040aa74e81fbfff48aee0b4fa
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-04-06 15:10:39 +0300

    added fields to ContactEntity

--- softgroup/basis/common-dao/common-dao-api/src/main/java/com/softgroup/common/dao/api/entities/ContactEntity.java
+++ softgroup/basis/common-dao/common-dao-api/src/main/java/com/softgroup/common/dao/api/entities/ContactEntity.java
@@ -1,7 +1,6 @@
 package com.softgroup.common.dao.api.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 /**
@@ -15,9 +14,59 @@ public class ContactEntity extends BaseEntity implements Serializable {
 
     private static final long serialVersionUID = 3180111570947462349L;
 
-    //todo add other fields later
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    private ProfileEntity profile;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone")
+    private String phone;
 
     public ContactEntity() {
     }
 
+    public ProfileEntity getProfile() {
+        return profile;
+    }
+
+    public void setProfile(ProfileEntity profile) {
+        this.profile = profile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ContactEntity)) return false;
+
+        ContactEntity that = (ContactEntity) o;
+
+        if (profile != null ? !profile.equals(that.profile) : that.profile != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        return phone != null ? phone.equals(that.phone) : that.phone == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = profile != null ? profile.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        return result;
+    }
 }
