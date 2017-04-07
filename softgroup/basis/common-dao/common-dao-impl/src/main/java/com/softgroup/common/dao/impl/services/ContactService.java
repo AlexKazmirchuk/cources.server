304d80175bf4de763ae4c911a2c06a12bc275daf
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-04-07 19:32:04 +0300

    added new query method to ContactService

--- softgroup/basis/common-dao/common-dao-impl/src/main/java/com/softgroup/common/dao/impl/services/ContactService.java
+++ softgroup/basis/common-dao/common-dao-impl/src/main/java/com/softgroup/common/dao/impl/services/ContactService.java
@@ -12,4 +12,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class ContactService extends
         CommonService<ContactEntity,ContactRepository> {
+
+    public ContactEntity findByPhone(String phone){
+        return getRepository().findByPhone(phone);
+    }
 }
