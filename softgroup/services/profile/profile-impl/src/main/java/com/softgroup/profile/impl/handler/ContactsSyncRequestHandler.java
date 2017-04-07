3f0d6880c7024c35327e16a7d712e348ff5c30dd
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-04-07 20:16:15 +0300

    added base logic to ContactSyncRequestHandler

--- softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/handler/ContactsSyncRequestHandler.java
+++ softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/handler/ContactsSyncRequestHandler.java
@@ -1,12 +1,22 @@
 package com.softgroup.profile.impl.handler;
 
+import com.softgroup.common.dao.api.entities.ContactEntity;
+import com.softgroup.common.dao.api.entities.ProfileEntity;
+import com.softgroup.common.dao.impl.services.ContactService;
+import com.softgroup.common.dao.impl.services.ProfileService;
+import com.softgroup.common.model.mapper.api.ModelMapper;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
+import com.softgroup.common.protocol.factories.MessageFactory;
 import com.softgroup.common.router.api.AbstractRequestHandler;
+import com.softgroup.profile.api.dto.ContactDTO;
 import com.softgroup.profile.api.message.ContactsSyncRequest;
 import com.softgroup.profile.api.message.ContactsSyncResponse;
 import com.softgroup.profile.api.router.ProfileRequestHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @author AlexKazmirchuk
@@ -18,6 +28,15 @@ public class ContactsSyncRequestHandler
         extends AbstractRequestHandler<ContactsSyncRequest,ContactsSyncResponse>
         implements ProfileRequestHandler {
 
+    @Autowired
+    private ProfileService profileService;
+
+    @Autowired
+    private ContactService contactService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
     public String getName() {
         return "contacts_sync";
@@ -25,10 +44,31 @@ public class ContactsSyncRequestHandler
 
     @Override
     public Response<ContactsSyncResponse> doHandle(Request<ContactsSyncRequest> msg) {
-        // todo implement later
+        ProfileEntity profile = profileService.getById(msg.getProfileID());
 
+        addContacts(profile, msg.getData().getAddedContacts());
+        deleteContacts(profile, msg.getData().getRemovedContacts());
 
-        //
-        return null;
+        return MessageFactory.createResponse(msg, new ContactsSyncResponse());
     }
+
+    private void addContacts(ProfileEntity profile, List<ContactDTO> addedContacts){
+        for (ContactDTO contactDTO : addedContacts) {
+            if(contactService.findByPhone(contactDTO.getPhone()) == null){
+                ContactEntity contactEntity = modelMapper.map(contactDTO,ContactEntity.class);
+                contactEntity.setProfile(profile);
+                contactService.add(contactEntity);
+            }
+        }
+    }
+
+    private void deleteContacts(ProfileEntity profile, List<ContactDTO> removedContacts){
+        for (ContactDTO contactDTO : removedContacts) {
+            ContactEntity contactEntity = contactService.findByPhone(contactDTO.getPhone());
+            if (contactEntity.getProfile().getId().equals(profile.getId())){
+                contactService.delete(contactEntity.getId());
+            }
+        }
+    }
+
 }
