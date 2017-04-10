5f3f9d95de4a77f79c407cbd4b80a39133536c86
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-04-10 09:35:58 +0300

    added needed fields to ProfileDTO

--- softgroup/services/profile/profile-api/src/main/java/com/softgroup/profile/api/dto/ProfileDTO.java
+++ softgroup/services/profile/profile-api/src/main/java/com/softgroup/profile/api/dto/ProfileDTO.java
@@ -1,8 +1,56 @@
 package com.softgroup.profile.api.dto;
 
 /**
- * Created by sasha on 24.02.17.
+ * @author AlexKazmirchuk
+ * @since 24.02.17.
  */
+
 public class ProfileDTO {
-    // impl. later
+
+    private String phoneNumber;
+    private String name;
+    private String status;
+    private String avatarUri;
+
+    public ProfileDTO(String phoneNumber, String name, String status, String avatarUri) {
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+        this.status = status;
+        this.avatarUri = avatarUri;
+    }
+
+    public ProfileDTO() {
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAvatarUri() {
+        return avatarUri;
+    }
+
+    public void setAvatarUri(String avatarUri) {
+        this.avatarUri = avatarUri;
+    }
 }
