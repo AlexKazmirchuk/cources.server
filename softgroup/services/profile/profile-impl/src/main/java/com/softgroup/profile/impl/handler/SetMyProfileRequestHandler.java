7f5290750275f5729006dd60264f9f53f2684452
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-04-10 10:13:21 +0300

    added base logic to SetMyProfileRequestHandler

--- softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/handler/SetMyProfileRequestHandler.java
+++ softgroup/services/profile/profile-impl/src/main/java/com/softgroup/profile/impl/handler/SetMyProfileRequestHandler.java
@@ -1,11 +1,15 @@
 package com.softgroup.profile.impl.handler;
 
+import com.softgroup.common.dao.api.entities.ProfileEntity;
+import com.softgroup.common.dao.impl.services.ProfileService;
 import com.softgroup.common.protocol.Request;
 import com.softgroup.common.protocol.Response;
+import com.softgroup.common.protocol.factories.MessageFactory;
 import com.softgroup.common.router.api.AbstractRequestHandler;
 import com.softgroup.profile.api.message.SetMyProfileRequest;
 import com.softgroup.profile.api.message.SetMyProfileResponse;
 import com.softgroup.profile.api.router.ProfileRequestHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -18,6 +22,9 @@ public class SetMyProfileRequestHandler
         extends AbstractRequestHandler<SetMyProfileRequest,SetMyProfileResponse>
         implements ProfileRequestHandler {
 
+    @Autowired
+    private ProfileService profileService;
+
     @Override
     public String getName() {
         return "set_my_profile";
@@ -25,10 +32,14 @@ public class SetMyProfileRequestHandler
 
     @Override
     public Response<SetMyProfileResponse> doHandle(Request<SetMyProfileRequest> msg) {
-        // todo implement later
 
+        ProfileEntity myProfile = profileService.getById(msg.getProfileID());
+        myProfile.setUpdateDateTime(System.currentTimeMillis());
+        myProfile.setStatus(msg.getData().getProfile().getStatus());
+        myProfile.setName(msg.getData().getProfile().getName());
+        myProfile.setAvatarUri(msg.getData().getProfile().getAvatarUri());
+        profileService.edit(myProfile);
 
-        //
-        return null;
+        return MessageFactory.createResponse(msg, new SetMyProfileResponse());
     }
 }
