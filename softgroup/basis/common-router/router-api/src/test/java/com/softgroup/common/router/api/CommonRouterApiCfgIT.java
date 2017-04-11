087821c87fa81f01a43d3e3497c8f1ee11e57508
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-04-11 20:03:05 +0300

    added tests for AbstractRouterHandler

--- softgroup/basis/common-router/router-api/src/test/java/com/softgroup/common/router/api/CommonRouterApiCfgIT.java
+++ softgroup/basis/common-router/router-api/src/test/java/com/softgroup/common/router/api/CommonRouterApiCfgIT.java
@@ -7,8 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.softgroup.common.router.api.util.Util.createRequest;
-import static com.softgroup.common.router.api.util.Util.createResponse;
+import static com.softgroup.common.router.api.util.Util.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -24,13 +23,13 @@ public class CommonRouterApiCfgIT {
     public List<RequestHandler> getRequestHandlerList(){
         RequestHandler handlerOne = mock(RequestHandler.class);
         when(handlerOne.getName()).thenReturn("test_command_one");
-        when(handlerOne.handle(createRequest("test_command_one",null)))
+        when(handlerOne.handle(REQUEST_WITH_TEST_COMMAND_ONE))
         .thenReturn(createResponse("test_command_one",null));
         when(handlerOne.handle(null)).thenThrow(new IllegalArgumentException());
 
         RequestHandler handlerTwo = mock(RequestHandler.class);
         when(handlerTwo.getName()).thenReturn("test_command_two");
-        when(handlerTwo.handle(createRequest("test_command_two",null)))
+        when(handlerTwo.handle(REQUEST_WITH_TEST_COMMAND_TWO))
                 .thenReturn(createResponse("test_command_two",null));
         when(handlerTwo.handle(null)).thenThrow(new IllegalArgumentException());
 
@@ -44,13 +43,13 @@ public class CommonRouterApiCfgIT {
     public List<RouterHandler> getRouterHandlerList(){
         RouterHandler handlerOne = mock(RouterHandler.class);
         when(handlerOne.getName()).thenReturn("test_type_one");
-        when(handlerOne.handle(createRequest(null,"test_type_one")))
+        when(handlerOne.handle(REQUEST_WITH_TEST_TYPE_ONE))
                 .thenReturn(createResponse(null,"test_type_one"));
         when(handlerOne.handle(null)).thenThrow(new IllegalArgumentException());
 
         RouterHandler handlerTwo = mock(RouterHandler.class);
         when(handlerTwo.getName()).thenReturn("test_type_two");
-        when(handlerTwo.handle(createRequest(null,"test_type_two")))
+        when(handlerTwo.handle(REQUEST_WITH_TEST_TYPE_TWO))
                 .thenReturn(createResponse(null,"test_type_two"));
         when(handlerTwo.handle(null)).thenThrow(new IllegalArgumentException());
 
@@ -77,6 +76,16 @@ public class CommonRouterApiCfgIT {
             public String getRouteKey(Request<?> msg) {
                 if (msg == null || msg.getHeader() == null) throw new IllegalArgumentException();
                 return msg.getHeader().getType();
+            }
+        };
+    }
+
+    @Bean
+    public AbstractRouterHandler<RequestHandler> getCommandRouterHandler(){
+        return new AbstractRouterHandler<RequestHandler>() {
+            @Override
+            public String getName() {
+                return "test_router_handler";
             }
         };
     }
