70ec1c2e76f606b306fd64636c2c1f7f9b928e7f
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-04-11 17:54:50 +0300

    added test methods for testing getHandler()

--- softgroup/basis/common-router/router-api/src/test/java/com/softgroup/common/router/api/AbstractFactoryTest.java
+++ softgroup/basis/common-router/router-api/src/test/java/com/softgroup/common/router/api/AbstractFactoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static com.softgroup.common.router.api.util.Util.createRequest;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
@@ -74,5 +75,73 @@ public class AbstractFactoryTest {
     @Test(expected = IllegalArgumentException.class)
     public void routerHandlerFactoryGetRouteKeyMethodWithNullHeader(){
         routerHandlerFactory.getRouteKey(new Request<>());
+    }
+
+    @Test
+    public void requestHandlerFactoryGetHandlerMethodTest(){
+        RequestHandler requestHandler = requestHandlerFactory.getHandler(
+                createRequest("test_command_one",null));
+
+        assertThat(requestHandler, notNullValue());
+        assertThat(requestHandler.getName(), is("test_command_one"));
+        assertThat(requestHandler, instanceOf(RequestHandler.class));
+
+        requestHandler = requestHandlerFactory.getHandler(
+                createRequest("test_command_two",null));
+        assertThat(requestHandler, notNullValue());
+        assertThat(requestHandler.getName(), is("test_command_two"));
+        assertThat(requestHandler, instanceOf(RequestHandler.class));
+
+        requestHandler = requestHandlerFactory.getHandler(
+                createRequest("not_existing_command",null));
+        assertThat(requestHandler, nullValue());
+
+        requestHandler = requestHandlerFactory.getHandler(
+                createRequest(null,null));
+        assertThat(requestHandler, nullValue());
+    }
+
+    @Test
+    public void routerHandlerFactoryGetHandlerMethodTest(){
+        RouterHandler routerHandler = routerHandlerFactory.getHandler(
+                createRequest(null,"test_type_one"));
+
+        assertThat(routerHandler, notNullValue());
+        assertThat(routerHandler.getName(), is("test_type_one"));
+        assertThat(routerHandler, instanceOf(RouterHandler.class));
+
+        routerHandler = routerHandlerFactory.getHandler(
+                createRequest(null,"test_type_two"));
+        assertThat(routerHandler, notNullValue());
+        assertThat(routerHandler.getName(), is("test_type_two"));
+        assertThat(routerHandler, instanceOf(RouterHandler.class));
+
+        routerHandler = routerHandlerFactory.getHandler(
+                createRequest(null,"not_existing_type"));
+        assertThat(routerHandler, nullValue());
+
+        routerHandler = routerHandlerFactory.getHandler(
+                createRequest(null,null));
+        assertThat(routerHandler, nullValue());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void requestHandlerFactoryGetHandlerMethodWithNullRequestTest(){
+        requestHandlerFactory.getHandler(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void requestHandlerFactoryGetHandlerMethodWithNullHeaderTest(){
+        requestHandlerFactory.getHandler(new Request<>());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void routerHandlerFactoryGetHandlerMethodWithNullRequestTest(){
+        routerHandlerFactory.getHandler(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void routerHandlerFactoryGetHandlerMethodWithNullHeaderTest(){
+        routerHandlerFactory.getHandler(new Request<>());
     }
 }
