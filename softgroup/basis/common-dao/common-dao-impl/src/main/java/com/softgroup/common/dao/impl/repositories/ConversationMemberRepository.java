564008bea99f5189ed0da476652f75f71afe5077
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-04-15 17:18:01 +0300

    added ConversationMemberEntity, his repo and service

---
+++ softgroup/basis/common-dao/common-dao-impl/src/main/java/com/softgroup/common/dao/impl/repositories/ConversationMemberRepository.java
@@ -0,0 +1,11 @@
+package com.softgroup.common.dao.impl.repositories;
+
+import com.softgroup.common.dao.api.entities.ConversationMemberEntity;
+
+/**
+ * @author AlexKazmirchuk
+ * @since 15.04.17.
+ */
+
+public interface ConversationMemberRepository extends BaseRepository<ConversationMemberEntity> {
+}
---
+++ softgroup/basis/common-dao/common-dao-impl/src/main/java/com/softgroup/common/dao/impl/services/ConversationMemberService.java
@@ -0,0 +1,13 @@
+package com.softgroup.common.dao.impl.services;
+
+import com.softgroup.common.dao.api.entities.ConversationMemberEntity;
+import com.softgroup.common.dao.impl.repositories.ConversationMemberRepository;
+
+/**
+ * @author AlexKazmirchuk
+ * @since 15.04.17.
+ */
+
+public class ConversationMemberService extends
+        CommonService<ConversationMemberEntity,ConversationMemberRepository> {
+}
